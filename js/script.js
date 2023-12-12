b4a3f56ac153a717006be49daef7c109519aaab2
OMaAR <131204654+omarmohamedali225@users.noreply.github.com>
2023-12-12 15:58:11 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// click icon checked input page question
+// const labal = document.querySelectorAll('.question .action label')
+// const form = document.querySelector('.question form')
+
+
+
+// form.addEventListener('submit')
+
+// labal.forEach(el => {
+//   const input = el.querySelector('input')
+//   const icon = el.querySelector('lord-icon')
+//   el.addEventListener('click',()=>{
+//     if(input.checked){
+//       console.log(input.value);
+//     }
+//   })
+// });
